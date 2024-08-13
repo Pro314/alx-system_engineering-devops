446a62bdb65c16b46b7120fe1a28ea1d14277931
Pro314 <apn.madlala@protonmail.com>
2024-08-13 14:45:41 +0200

    Advanced API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""Module for task 0"""
+""" gets the number of subs for a subredit """
+import json
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
+    """function that takes a subredit and returns number of subs"""
+    user_agent = {"User-Agent": "unix:0-subs.py:v1.0"}
+    data = requests.get("https://www.reddit.com/r/{}/about.json"
+                        .format(subreddit),
+                        headers=user_agent,
+                        allow_redirects=False)
+    if data.status_code != 200:
         return 0
-
-    return sub_info.json().get("data").get("subscribers")
+    json_data = data.json()
+    results = json_data.get("data").get("subscribers")
+    return results
