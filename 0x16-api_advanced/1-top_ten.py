af5b8928dcfee0a16119393b3561393382f6e426
Pro314 <apn.madlala@protonmail.com>
2024-08-13 14:51:08 +0200

    Advanced API

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-"""Module for task 1"""
+"""gets top 10 post on subredit"""
+import json
+import requests
 
 
 def top_ten(subreddit):
-    """Queries the Reddit API and returns the top 10 hot posts
-    of the subreddit"""
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
-                            .format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code >= 300:
-        print('None')
-    else:
-        [print(child.get("data").get("title"))
-         for child in sub_info.json().get("data").get("children")]
+    """gets the top 10 post of a subreddit"""
+    user_agent = {"User-Agent": "unix:0-subs.py:v1.0"}
+    data = requests.get("https://www.reddit.com/r/{}/hot/.json"
+                        .format(subreddit),
+                        headers=user_agent,
+                        allow_redirects=False)
+    if data.status_code != 200:
+        print("None")
+        return
+    json_data = data.json().get("data").get("children", [])
+    if len(json_data) == 0:
+        print("None")
+        return
+    for post in json_data[0:10]:
+        print(post.get("data").get("title"))
